d5f266f3cda5c62db1e6ea0b62ce723cdee30b68
Yashodhan Ketkar <kykyashodhan@gmail.com>
2023-12-12 00:19:35 +0530

    Added dist folder for render

---
+++ dist/routes/user/index.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UserRouter = void 0;
+const express_1 = require("express");
+const auth_1 = require("../../middlewares/auth");
+const controller_1 = require("./controller");
+const router = (0, express_1.Router)();
+exports.UserRouter = router;
+const userController = new controller_1.UserController();
+// get routes
+router.get("/me", [auth_1.Auth], userController.me);
+router.get("/stats", userController.stats);
+router.get("/stats/:id", userController.statsByID);
+// post routes
+router.post("/register", userController.register);
+router.post("/login", userController.login);
